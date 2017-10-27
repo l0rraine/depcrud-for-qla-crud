23694089c45f7e124c059a32afda8c3e3af4cfcc
l0rraine <idn.lee@gmail.com>
2017-10-27 10:33:39 +0800

    fix typo

--- src/routes/depcrud.php
+++ src/routes/depcrud.php
@@ -2,9 +2,9 @@
 /*****************单位管理*****************/
 Route::group(['prefix' => 'department'], function () {
     Route::get('/', 'DepCrudController@getIndex')->name(config('qla.depcrud.route_name_prefix', 'Crud.Dep') . '.index');
-    Route::get('indexJson', 'DepCrudController@indexJson')->name(config('qla.depcrud.route_name_prefix', 'Crud.Dep') . 'indexJson');
+    Route::get('indexJson', 'DepCrudController@indexJson')->name(config('qla.depcrud.route_name_prefix', 'Crud.Dep') . '.indexJson');
     Route::get('add', 'DepCrudController@getAdd')->name(config('qla.depcrud.route_name_prefix', 'Crud.Dep') . '.add');
-    Route::post('add', 'DepCrudController@postAdd')->name(config('qla.depcrud.route_name_prefix', 'Crud.Dep') . '.post');
+    Route::post('add', 'DepCrudController@postAdd')->name(config('qla.depcrud.route_name_prefix', 'Crud.Dep') . '.add.post');
     Route::get('edit/{id}', 'DepCrudController@getEdit')->name(config('qla.depcrud.route_name_prefix', 'Crud.Dep') . '.edit');
     Route::post('edit', 'DepCrudController@postEdit')->name(config('qla.depcrud.route_name_prefix', 'Crud.Dep') . '.edit.post');
     Route::post('sort/save', 'DepCrudController@doSaveSortId')->name(config('qla.depcrud.route_name_prefix', 'Crud.Dep') . '.saveSort');
